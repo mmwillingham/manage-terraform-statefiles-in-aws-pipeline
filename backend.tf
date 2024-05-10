f022b9fc896f5c08c559bcbc9da7eae8cf4ebccb
Martin Willingham <37187589+mmwillingham@users.noreply.github.com>
2024-05-10 09:10:52 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "tfbackend-bucket"
     key            = "terraform.tfstate"
-    region         = "eu-central-1"
+    region         = "us-east-2"
     dynamodb_table = "tfstate-lock"
     encrypt        = true
   }
